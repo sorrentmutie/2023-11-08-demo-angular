39c3fb6de5e733098a24beb8a1addaf54f3fdc80
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2023-11-09 12:52:18 +0100

    products catalog class

--- src/app/products/services/product-catalog.ts
+++ src/app/products/services/product-catalog.ts
@@ -1,17 +1,20 @@
 import { Product } from "../models/product";
 
 export class ProductCatalog {
+   private products: Product[] =  [
+    {id: 1, name: "Frigorifero", price: 1000,
+     isAvailable: true, releaseDate: new Date(),
+     image: "https://cdn.freebiesupply.com/logos/large/2x/angular-icon-1-logo-png-transparent.png"},
+     {id: 2, name: "TV", price: 1000,
+     isAvailable: true, releaseDate: new Date(),
+     image: "https://www.codeur.com/blog/wp-content/uploads/2018/12/logo-angular.png"}
 
-    getProducts(): Product[] {
-        return [
-          {id: 1, name: "Frigorifero", price: 1000,
-           isAvailable: true, releaseDate: new Date(),
-           image: "https://cdn.freebiesupply.com/logos/large/2x/angular-icon-1-logo-png-transparent.png"},
-           {id: 2, name: "TV", price: 1000,
-           isAvailable: true, releaseDate: new Date(),
-           image: "https://www.codeur.com/blog/wp-content/uploads/2018/12/logo-angular.png"}
-    
-        ];
-      }
+  ];
+   addProduct(product: Product) {
+      this.products.push(product);
+   }
+   getProducts(): Product[] {
+        return this.products;
+    }
     
 }
