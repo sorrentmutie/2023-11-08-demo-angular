a4c4e89ed6a0c47192b55eef2363b8ef4ef05e85
sorrentmutie <salvatore.sorrentino@live.com>
2023-11-09 09:43:55 +0100

    ngFor

--- src/app/products/components/products-page/products-page.component.ts
+++ src/app/products/components/products-page/products-page.component.ts
@@ -18,10 +18,10 @@ export class ProductsPageComponent {
     return [
       {id: 1, name: "Frigorifero", price: 1000,
        isAvailable: true, releaseDate: new Date(),
-       image: ""},
+       image: "https://cdn.freebiesupply.com/logos/large/2x/angular-icon-1-logo-png-transparent.png"},
        {id: 2, name: "TV", price: 1000,
        isAvailable: true, releaseDate: new Date(),
-       image: ""}
+       image: "https://www.codeur.com/blog/wp-content/uploads/2018/12/logo-angular.png"}
 
     ];
   }
